3fb0c9392fc1c57f1341857f194d833c9ce959da
k-seungtae <seong954t.temp@gmail.com>
2022-04-30 17:40:34 +0900

    feature: 과제용 소스코드 추가

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -2,6 +2,18 @@ class Calculator {
   plus = (a, b) => {
     return a + b;
   };
+  isNegative = (a) => {
+    return a < 0;
+  };
+  getDate = () => {
+    return new Date();
+  };
+  abs = (a) => {
+    return Math.abs(a);
+  };
+  customCalculation = (callback, ...args) => {
+    callback(...args);
+  };
 }
 
 export default Calculator;
