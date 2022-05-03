6740b5a5671a7543cd4625a3fa6ef4317403a1be
Dong young <yrrho2@naver.com>
2022-05-04 00:45:04 +0900

    team-11-201802086

---
+++ src/__tests__/Calculator_201802086.spec.js
@@ -0,0 +1,34 @@
+import Calculator from "../js/calculator/Calculator";
+
+beforeAll(() => {
+  console.log("모든 테스트를 실행하기 전 한번만 실행된다.");
+})
+afterAll(() => {
+  console.log("모든 테스트가 완료된 후 한번만 실행된다.");
+})
+beforeEach(() => {
+  console.log("각 테스트를 실행하기 전 실행된다.");
+})
+afterEach(() =>{
+  console.log("각 테스트가 완료된 후 실행된다.");
+})
+
+describe("음수 양수 판단", () => {
+    const cal = new Calculator();
+  test("isNegative에 7을 넣으면 false를 반환한다.", () => {
+    expect(cal.isNegative(7)).toBeFalsy();
+  });
+  test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
+    expect(cal.isNegative(-3)).toBeTruthy();
+  });
+  test("isNegative에 0을 넣으면 true가 아니다.", () => {
+    expect(cal.isNegative(0)).not.toBeTruthy();
+  });
+});
+
+describe("Date 클래스 판단하기", () => {
+    const cal = new Calculator();
+  test("getDate를 하면 Date객체가 나온다.", () => {
+    expect(cal.getDate()).toBeInstanceOf(Date);
+  });
+});
