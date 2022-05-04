8402e55b7522680ce74b9104069af0565b2a51a4
tnghwk0661 <tnghwk0661@gmail.com>
2022-05-04 18:53:02 +0900

    calculator

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -2,6 +2,21 @@ class Calculator {
   plus = (a, b) => {
     return a + b;
   };
+
+  minus = (a, b) => {
+    return a - b;
+  };
+
+  multiply = (a, b) => {
+    return a * b;
+  };
+
+  divide = (a, b) => {
+    if (b == 0){
+      throw new DivideZeroError();
+    }
+    return a / b;
+  };
 }
 
 export default Calculator;
