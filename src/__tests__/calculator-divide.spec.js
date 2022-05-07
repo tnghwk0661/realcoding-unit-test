3d9ef5da2c477db9ff4455a39cf15eb98733feb2
tnghwk0661 <tnghwk0661@gmail.com>
2022-05-07 20:52:25 +0900

    div0

--- src/__tests__/calculator-divide.spec.js
+++ src/__tests__/calculator-divide.spec.js
@@ -1,16 +1,22 @@
 import Calculator from "../js/calculator/Calculator";
 
 describe("test div.", () => {
-  
+  test("11 / 0 ", ()=>{
+    const calculator = new Calculator();
+    expect(() => calculator.divide(11, 0)).toThrow("ZeroDivisionError");
+  });
   test("6 / 3 == 2.", () => {
-    expect(6 / 2).toEqual(2);
+    const calculator = new Calculator();
+    expect(calculator.divide(6, 3)).toEqual(2);
   });
   
   test("1024 / 1024 == 1.", () => {
-    expect(1024 / 1024).toEqual(1);
+    const calculator = new Calculator();
+    expect(calculator.divide(1024, 1024)).toEqual(1);
   });
   
   test("121 / 11 == 11.", () => {
-    expect(121 / 11).toEqual(11);
+    const calculator = new Calculator();
+    expect(calculator.divide(121, 11)).toEqual(11);
   });
 });
