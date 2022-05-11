d3c2f35ee676ae72d5176ca5220d07698c82b537
sungwoochang <tjd5024@gmail.com>
2022-05-11 15:45:45 +0900

    team-11-201802109-추가과제

--- src/__tests__/201802109/Calculator.spec.js
+++ src/__tests__/201802109/Calculator.spec.js
@@ -38,4 +38,28 @@ describe("Date 클래스 판단하기", () => {
     expect(calculator.getDate()).toBeInstanceOf(Date);
   });
 });
+
+describe("가상 함수 테스트해보기", () =>{
+    test("customCalculation는 전달 된 함수를 호출시킨다.",() =>{
+        const calculator = new Calculator();
+        const mockFunction = jest.fn();
+        calculator.customCalculation(mockFunction);
+        expect(mockFunction).toHaveBeenCalled();
+    });
+
+    test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달 된 함수가 인자 1, 2, 3을 받아 호출한다.", () => {
+        const calculator = new Calculator();
+        const mockFunction = jest.fn();
+        calculator.customCalculation(mockFunction, 1, 2, 3);
+        expect(mockFunction).toHaveBeenCalledWith(1, 2, 3);
+    });
+    test("abs함수는 Math.abs를 호출한다.", () => {
+        const calculator = new Calculator();
+        const mockMathAbs = jest.spyOn(Math, "abs");
+        calculator.abs(3);
+        expect(mockMathAbs).toHaveBeenCalled();
+    });
+
+
+})
   
